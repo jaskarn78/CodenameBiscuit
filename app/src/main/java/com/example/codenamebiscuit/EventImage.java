673cca470ad6db49d9819fcc02616acf955ea28e
jaskarnjagpal <jagpal@csus.edu>
2017-02-03 09:12:52 -0800

    -Added event image onto the event list -Moved QueryEventList into a separate class -Created class DatabaseHelper class to store image data into sqlite -

---
+++ app/src/main/java/com/example/codenamebiscuit/EventImage.java
@@ -0,0 +1,15 @@
+package com.example.codenamebiscuit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class EventImage extends AppCompatActivity {
+    private ImageView eventImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_image);
+    }
+}
